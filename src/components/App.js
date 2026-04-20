ad2c771e5cf2c7c08b5a1caff38c4967bcace163
Ritik Sahoo <ritiksahoo380@gmail.com>
2026-04-20 16:01:49 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [count, setCount] = useState()
+  let [count, setCount] = useState(0)
 
   return (
     <div>
